5eb5dd07b2ff4b5968d3fa4b2c9910b8053e6f75
astrazent <phannguyen2300@gmail.com>
2025-03-25 23:34:07 +0700

    merge code branch lap 2

--- webquanao/application/logs/log-2025-03-25.php
+++ webquanao/application/logs/log-2025-03-25.php
@@ -6,3 +6,7 @@ ERROR - 2025-03-25 11:57:38 --> Đặt hàng thành công!
 ERROR - 2025-03-25 12:08:27 --> Đặt hàng thành công!
 ERROR - 2025-03-25 13:29:37 --> 404 Page Not Found: Faviconico/index
 ERROR - 2025-03-25 15:44:13 --> Đặt hàng thành công!
+ERROR - 2025-03-25 16:13:59 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-03-25 16:14:10 --> The upload path does not appear to be valid.
+ERROR - 2025-03-25 16:15:04 --> The upload path does not appear to be valid.
+ERROR - 2025-03-25 16:20:59 --> The upload path does not appear to be valid.
