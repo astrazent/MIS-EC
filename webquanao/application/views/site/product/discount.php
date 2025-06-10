266d434df3c3a37b993c46b23681a98903180ed1
LapVT2k3 <kendyvu2k3@gmail.com>
2025-06-10 13:30:03 +0700

    .

--- webquanao/application/views/site/product/discount.php
+++ webquanao/application/views/site/product/discount.php
@@ -51,14 +51,14 @@
 										</a>
 									</div>
 								</div>
-								<?php if ($value->discount > 0 || $value->price < $value->origin_price) {
+								<!-- <?php if ($value->discount > 0 || $value->price < $value->origin_price) {
 									$new_price = $value->price - $value->discount; ?>
 									<p><span class='price text-right'><?php echo number_format($new_price); ?> VNĐ</span> <del class="product-discount"><?php echo number_format($value->price); ?> VNĐ</del></p>
 								<?php } else { ?>
 									<p><span class='price text-right'><?php echo number_format($value->origin_price); ?> VNĐ</span></p>
 								<?php	} ?>
 								<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true" title="Số lượt xem"></span> <?php echo $value->view; ?> <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true" title="Số lượng đặt mua"><?php echo $value->buyed; ?></p>
-								<a href="<?php echo base_url('cart/add/' . $value->id); ?>"><button class='btn btn-info'><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Thêm giỏ hàng</button></a>
+								<a href="<?php echo base_url('cart/add/' . $value->id); ?>"><button class='btn btn-info'><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Thêm giỏ hàng</button></a> -->
 							</div>
 						</div>
 					<?php } ?>
